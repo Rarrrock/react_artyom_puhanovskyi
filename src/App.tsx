0adf1273f1e98a02df097bb61d2ae894ff70cf13
Артём Рассветный <artemius.morningstar@gmail.com>
2024-05-14 12:00:59 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, {FC} from 'react';
 import './App.css';
+import Character from "./Components/character/Character";
 
 const App: FC = () => {
   return (
       <div>
-
+          <Character></Character>
+          <Character/>
       </div>
   );
 };
---
+++ src/Components/character/Character.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+
+const Character: FC = () => {
+    return (
+        <div>
+            <h2>bart</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png" alt="bart"/>
+        </div>
+    );
+};
+
+export default Character;
